74c1857361250fe60a3843e58c49aa59250be6db
Naseef Sami <cmnsami@gmail.com>
2019-12-03 15:43:36 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,3 +4,6 @@
 first_number = 42
 second_number = 25
 sum = first_number + second_number
+difference = first_number - second_number
+product = first_number ** second_number
+quotient = first_number 
