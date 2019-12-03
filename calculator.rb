1150889b41bcdf9ff7f65b26c3d25a4a3560a6a6
Naseef Sami <cmnsami@gmail.com>
2019-12-03 15:46:23 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -5,5 +5,5 @@ first_number = 42
 second_number = 25
 sum = first_number+second_number
 difference = first_number-second_number
-product = first_number**second_number
+product = first_number*second_number
 quotient = first_number/second_number
